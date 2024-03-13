3cdca2229049379b830837c59f64a52c77d4e61c
Ben Klinovsky <bnk989@gmail.com>
2024-03-13 11:43:24 +0200

    Add mobile hamburger manu

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,17 +1,27 @@
+const { useRef } = React
 const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
-    return <header className="app-header">
+    const headerRef = useRef()
+
+    const toggleMenu = () => {
+        headerRef.current.classList.toggle('menu-open')
+    }
+
+    return <header ref={headerRef} className="app-header">
         <Link className="logo flex align-center" to="/">
             <img src="assets/img/appSus-logo.png" alt="logo" />
             <h3>AppSus</h3>
         </Link>
-        <nav>
+        
+        <nav onClick={toggleMenu}>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/mail">Mail</NavLink>
             <NavLink to="/note">Note</NavLink>
         </nav>
+        <button className="toggle-menu-btn" onClick={toggleMenu} type="button">☰</button>
+        <div className="backdrop" onClick={toggleMenu}></div>
     </header>
 }
