5a6214e77c068132cf94f9d9af9cd900c92cab75
TomTzadka <tomtzadka@gmail.com>
2024-03-15 19:04:41 +0200

    basic design

--- apps/mail/cmps/MailSideBar.jsx
+++ apps/mail/cmps/MailSideBar.jsx
@@ -1,36 +1,30 @@
-const { useState, useEffect } = React
-const { Link, useSearchParams } = ReactRouterDOM
-const { useParams, } = ReactRouter;
+const { useState, useEffect } = React;
+const { Link, useSearchParams } = ReactRouterDOM;
+const { useParams } = ReactRouter;
 
+export function MailSideBar({ setCurrFilter }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
 
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
 
-export function MailSideBar({setCurrFilter}) {
-    const [isLoading, setIsLoading] = useState(true)
-    const params = useParams();
-
-    useEffect(()=>{
-        setIsLoading(false)
-
-    },[])
-
-
-
-
-
-
-
-    if (isLoading) return <div>Loading side bar..</div>
-    return <ul className="mail-side-bar">
-        <Link to={`/mail/inbox`}>
-            <li onClick={()=>setCurrFilter('inbox')}>Inbox</li>
-        </Link>
-        <Link to={`/mail/starred`}>
-            <li onClick={()=>setCurrFilter('starred')}>Starred</li>
-        </Link>
-        <Link to={`/mail/trash`}>
-            <li>Trash</li>
-        </Link>
-        </ul>
-        
+  if (isLoading) return <div>Loading side bar..</div>;
+  return (
+    <ul className="mail-side-bar">
+      <Link to={`/mail/inbox`}>
+        <i className="fa-solid fa-trash inline"></i>
+        <li className="inline" onClick={() => setCurrFilter("inbox")}>
+          Inbox
+        </li>
+      </Link>
+      <Link to={`/mail/starred`}>
+        <li onClick={() => setCurrFilter("starred")}>Starred</li>
+      </Link>
+      <Link to={`/mail/trash`}>
+        <li>Trash</li>
+      </Link>
+    </ul>
+  );
 }
-
